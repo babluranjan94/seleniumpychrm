7d4eedaceabcb39e4a7699d6ab9f82e91974ff74
babluranjan94 <45945980+babluranjan94@users.noreply.github.com>
2019-09-27 00:25:35 +0530

    Update Xtudylogin.py

--- Xtudylogin.py
+++ Xtudylogin.py
@@ -1,31 +1,71 @@
 
 from selenium import webdriver
-driver = webdriver.Chrome(executable_path=r"D:\drivers\chromedriver.exe")
+from selenium.webdriver.common.keys import Keys
 import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+
+
+driver = webdriver.Chrome(executable_path=r"D:\drivers\chromedriver.exe")
 
 
 def applogin (url, userid, password, inputuser, inputpassword, submitbutton):
 
     driver.get(url)
-    time.sleep(5)
+    driver.maximize_window()
+    WebDriverWait(driver, 5).until(EC.presence_of_all_elements_located((By.XPATH, inputuser)))
     driver.find_element_by_xpath(inputuser).send_keys(userid)
     driver.find_element_by_xpath(inputpassword).send_keys(password)
     driver.find_element_by_xpath(submitbutton).click()
-    time.sleep(5)
-    #if driver.current_url('http://beta.xtudy.xyz/dashboard/dashboard.php'):
-    return driver.current_url
-    #else:
-    #   return 'FAIL'
+    time.sleep(2)
 
 def logout():
+    Button = driver.find_element_by_xpath(r'/html/body/div[1]/div[2]/nav/div/div[3]/ul/li/a/i')
+    driver.execute_script("arguments[0].scrollIntoView();", Button)
     driver.find_element_by_xpath('/html/body/div[1]/div[2]/nav/div/div[3]/ul/li/a/i').click()
-    time.sleep(5)
-    #if driver.current_url('http://beta.xtudy.xyz/dashboard/login.php'):
+    time.sleep(2)
     driver.close()
-    return driver.current_url
-    #else:
-        #return 'fail'
-def add_student(admission,add, student_xpath,value):
-    driver.find_element_by_xpath(admission).click()
-    driver.find_element_by_xpath(add).click()
-    driver.find_element_by_xpath(student_xpath).send_keys(value)
+
+
+def CLICK(xpath):
+    driver.find_element_by_xpath(xpath).click()
+
+
+
+
+def SETTEXT(xpath,data):
+    driver.find_element_by_xpath(xpath).send_keys(data)
+
+
+
+def down():
+    home = driver.find_element_by_xpath(r'/html/body/div[1]/div[2]/footer/div/nav/ul/li[1]/a')
+    driver.execute_script("arguments[0].scrollIntoView();", home)
+
+
+
+def dropdown(xpath,value):
+    driver.find_element_by_xpath(xpath).click()
+    driver.find_element_by_xpath(xpath+'/option[@value="'+value+'"]').click()
+
+
+def Wait_sec(sec):
+    time.sleep(sec)
+
+
+
+def SET_DATE(xpath,data):
+    element = driver.find_element_by_xpath(xpath)
+    element.send_keys(Keys.CONTROL + "a")
+    element.send_keys(Keys.DELETE)
+    element.send_keys(data)
+    element.send_keys(Keys.ENTER)
+
+
+
+def GET_TEXT(xpath):
+    data = driver.find_element_by_xpath(xpath).text
+    return data
